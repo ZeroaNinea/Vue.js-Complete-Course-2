4e937dbb3dabe7ff952708bd886315ce60c4db1f
Catgirl <zeroaninea@gmail.com>
2026-02-26 19:05:58 +0400

    button to contact added

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToContact() {
+  router.push('/contact')
+}
+</script>
+
 <template>
   <div>Home Page</div>
+  <button class="btn btn-info" @click="goToContact">Contact Us</button>
 </template>
