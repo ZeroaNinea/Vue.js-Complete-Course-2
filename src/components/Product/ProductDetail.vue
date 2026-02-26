ebfaa2fb2b05c04c0d5333dd552a1260f4d06561
Catgirl <zeroaninea@gmail.com>
2026-02-26 18:26:48 +0400

    productId passed

--- src/components/Product/ProductDetail.vue
+++ src/components/Product/ProductDetail.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+console.log(route.params)
+</script>
+
 <template>
   <div>
     <h1>Product Detail</h1>
+    <p>Product ID: {{ route.params.productId }}</p>
   </div>
 </template>
--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -3,9 +3,18 @@
     <h1>Product List</h1>
     <p>Here is the list of products:</p>
     <ul>
-      <li>Product 1</li>
+      <!-- <li>Product 1</li>
       <li>Product 2</li>
-      <li>Product 3</li>
+      <li>Product 3</li> -->
+      <li>
+        <router-link class="text-success" to="/product/1">Product 1</router-link>
+      </li>
+      <li>
+        <router-link class="text-success" to="/product/2">Product 2</router-link>
+      </li>
+      <li>
+        <router-link class="text-success" to="/product/3">Product 3</router-link>
+      </li>
     </ul>
   </div>
 </template>
