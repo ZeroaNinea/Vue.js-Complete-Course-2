221d32674297412f0c573377f3bd743b45273a6b
Catgirl <zeroaninea@gmail.com>
2026-02-26 18:05:09 +0400

    links added

--- src/components/Layout/AppHeader.vue
+++ src/components/Layout/AppHeader.vue
@@ -16,10 +16,12 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <!-- <a class="nav-link" href="#">Link</a> -->
+            <router-link class="nav-link" to="/contact">Contact</router-link>
           </li>
           <li class="nav-item dropdown">
             <a
