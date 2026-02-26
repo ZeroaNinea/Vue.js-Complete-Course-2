6347fa08e55d52084571f45f527ac0a6e3e84610
Catgirl <zeroaninea@gmail.com>
2026-02-26 16:19:16 +0400

    routes added

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: () => import('../components/Home/HomePage.vue'),
+    },
+    {
+      path: '/contact',
+      component: () => import('../components/Home/ContactUs.vue'),
+    },
+  ],
 })
 
 export default router
