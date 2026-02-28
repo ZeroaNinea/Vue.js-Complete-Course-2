c537069379423279e67b2ebda5cc96e87dace58d
Catgirl <zeroaninea@gmail.com>
2026-02-28 21:59:18 +0400

    game buttons added

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,22 @@ import UsersApi from './components/Users/UsersApi.vue'
   <AppHeader />
   <router-view></router-view>
   <UsersApi />
+  <div class="text-center">
+    <p>Let's play the game</p>
+    <h2 class="text-primary pb-3">Current Score:</h2>
+    <span class="text-primary pb-3">Max Score:</span>
+  </div>
+  <div class="row">
+    <div class="col-4 offset-1">
+      <button class="form-control btn btn-success p-4">Increment</button>
+    </div>
+    <div class="col-4 offset-1">
+      <button class="form-control btn btn-danger p-4">Decrement</button>
+    </div>
+    <div class="col-6 offset-3 pt-2">
+      <button class="form-control btn btn-warning p-4">Random</button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
