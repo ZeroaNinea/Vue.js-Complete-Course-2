3a557079dc5c436e870d6cb12f405ef7092e46d2
Catgirl <zeroaninea@gmail.com>
2026-02-28 22:17:32 +0400

    increment decrement random functions added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
 import AppHeader from './components/Layout/AppHeader.vue'
 import UsersApi from './components/Users/UsersApi.vue'
+import { useGameStore } from './stores/game-store'
+
+const gameStore = useGameStore()
+
+function increment() {
+  gameStore.score += Math.floor(Math.random() * 30) + 1
+}
+
+function decrement() {
+  gameStore.score -= Math.floor(Math.random() * 30) + 1
+}
+
+function random() {
+  const isIncrement = Math.random() > 0.5
+
+  if (isIncrement) {
+    increment()
+  } else {
+    decrement()
+  }
+}
 </script>
 
 <template>
@@ -9,17 +30,17 @@ import UsersApi from './components/Users/UsersApi.vue'
   <UsersApi />
   <div class="text-center">
     <p>Let's play the game</p>
-    <h2 class="text-primary pb-3">Current Score:</h2>
-    <span class="text-primary pb-3">Max Score:</span>
+    <h2 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h2>
+    <span class="text-primary pb-3">Max Score: {{ gameStore.maxHeath }}</span>
   </div>
   <div class="row">
     <div class="col-4 offset-1">
-      <button class="form-control btn btn-success p-4">Increment</button>
+      <button @click="increment()" class="form-control btn btn-success p-4">Increment</button>
     </div>
-    <div class="col-4 offset-1">
+    <div @click="decrement()" class="col-4 offset-1">
       <button class="form-control btn btn-danger p-4">Decrement</button>
     </div>
-    <div class="col-6 offset-3 pt-2">
+    <div @click="random()" class="col-6 offset-3 pt-2">
       <button class="form-control btn btn-warning p-4">Random</button>
     </div>
   </div>
